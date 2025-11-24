e956575f060a49fa5698efe5ffa64ac22b63eabe
Fakezin DLL <josevictorsouza2006@gmail.com>
2025-11-24 20:37:36 -0300

    Ajuste no back-end para o deploy.
    Ajustei e criei alguns arquivos para fazer o deploy do projeto.

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api import routes
 
-app = FastAPI(title="CipherSecurity API")
+app = FastAPI(title="CypherSecurity API")
 
 app.add_middleware(
     CORSMiddleware,
